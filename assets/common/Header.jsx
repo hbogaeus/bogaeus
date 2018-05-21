f1a73490cd188279f5f5e2c505c1de4bedcb84ea
Henry Bogaeus <henry.bogaeus@gmail.com>
2018-05-21 12:45:46 +0200

    Modified header

--- assets/common/Header.jsx
+++ assets/common/Header.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import style from "./style.css";
+import style from "./Header.css";
 import indexStyle from "../index.css";
 
 const Header = () => (
-  <div className={style.header}>
-    <NavLink className={style.headerItem} key={1} to="/">
-      Bogaeus
-    </NavLink>
-    <NavLink className={style.headerItem} key={2} to="/beats">
-      Beats
-    </NavLink>
+  <div className={style.main}>
+    <ul>
+      <li className={style.item}>
+        <NavLink
+          className={style.link}
+          activeClassName={style.selected}
+          key={1}
+          to="/"
+          exact
+        >
+          Bogaeus
+        </NavLink>
+      </li>
+      <li className={style.item}>
+        <NavLink
+          className={style.link}
+          activeClassName={style.selected}
+          key={2}
+          to="/beats"
+        >
+          Beats
+        </NavLink>
+      </li>
+    </ul>
+    {/* 
     <NavLink className={style.headerItem} key={3} to="/musicforprogramming">
       Music For Programming
     </NavLink>
+    */}
   </div>
 );
 
