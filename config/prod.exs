250573854c76710d42d2acfd522916f39d0c421e
Henry Bogaeus <henry.bogaeus@gmail.com>
2018-05-27 20:55:56 +0200

    Changed production conf

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bogaeus, BogaeusWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "bogaeus.com", port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
